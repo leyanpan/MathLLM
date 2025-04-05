1505946c580d8cb82583864f0d950de2e7593ad1
greg-const <86847050+greg-const@users.noreply.github.com>
2025-04-05 12:41:58 -0400

    Update dsp_test_new.py

--- temp_code/dsp_test_new.py
+++ temp_code/dsp_test_new.py
@@ -14,7 +14,7 @@ def get_project_and_lean_path():
     return cwd, p
 
 
-client = genai.Client(api_key='AIzaSyA4MJWtYBhMQETBpv85m1oq4iXR-Nr0CF8')
+client = genai.Client(api_key='')
 model='gemini-2.5-pro-exp-03-25'
 
 def lean_block_to_tac_list(block):
